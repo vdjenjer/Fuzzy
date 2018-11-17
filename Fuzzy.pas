b3a61e959bfa2feb1676585745ac66803b0280ef
vdjenjer <32304630+vdjenjer@users.noreply.github.com>
2018-11-17 19:18:36 +0500

    1st stable

---
+++ Fuzzy.pas
@@ -0,0 +1,30 @@
+﻿unit Fuzzy;
+
+interface
+type TrapMF = auto class
+  name: string;
+  a, b, c, d: real;
+  function fun(x: real): real;
+  begin
+    if x <= a then
+      result := 0
+    else if x <= b then 
+      result := (x - a) / (b - a)
+    else if x <= c then
+      result := 1
+    else if x <= d then
+      result := (d - x) / (d - c)
+    else
+      result := 0;
+  end;
+end;
+  
+type Lingvo = class
+  
+end;
+    
+implementation
+
+begin
+
+end.
---
+++ TestFuzzy.pas
@@ -0,0 +1,5 @@
+﻿uses Fuzzy;
+begin
+  var f := new Trapmf(1, 2, 3, 4);
+  
+end.
