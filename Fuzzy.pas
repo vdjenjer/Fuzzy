b1cf7e0c75dc674624c71d2faa56c206220f6a3f
vdjenjer <32304630+vdjenjer@users.noreply.github.com>
2018-11-19 11:03:46 +0500

    graph lingvar

--- Fuzzy.pas
+++ Fuzzy.pas
@@ -1,10 +1,44 @@
 ﻿unit Fuzzy;
 
 interface
-type TrapMF = auto class
-  name: string;
-  a, b, c, d: real;
-  function fun(x: real): real;
+
+uses GraphABC;
+
+type
+  TrapMF = auto class
+    name: string;
+    a, b, c, d: real;
+    function fun(x: real): real;
+    procedure plot(a, b: real; c: color);
+  end;
+
+type
+  Lingvar = auto class
+    name: string;
+    a, b: real;
+    funcs: List<TrapMF>; 
+    procedure plot;
+  end;
+
+implementation
+
+
+var
+  colorsmf := new List<color>;
+
+
+procedure Lingvar.plot;
+begin
+  for var i := 0 to funcs.Count - 1 do
+  begin
+    funcs[i].plot(-1, 1, colorsmf[i mod colorsmf.Count])
+  end;
+end;
+
+
+
+function TrapMF.fun(x: real): real;
+begin
   begin
     if x <= a then
       result := 0
@@ -18,15 +52,29 @@ type TrapMF = auto class
       result := 0;
   end;
 end;
-  
-type Lingvar = auto class
-  name: string;
-  opr: (real, real);
-  funcs: List<TrapMF>; 
+
+///Строит одну MF на отрезке [a, b]
+procedure TrapMF.plot(a, b: real; c: color);
+var
+  Hu, Hv, rx, ry, Sx, Sy: real;
+begin
+  (rx, ry) := ((b - a) * 1.1, 1.1);
+  (Sx, Sy) := (Window.Width / rx, Window.Height / ry);
+  (Hu, Hv) := (-a * Sx, Window.Height - 2);
+  var dx := 0.01;
+  var p := new List<Point>;
+  var x := a;
+  while x < b do
+  begin
+    var y := -fun(x);
+    p.Add(pnt(round(x * Sx + Hu), round(-fun(x) * Sy + Hv)));
+    x += dx;
+  end;
+  pen.Color := c;
+  Polyline(p.ToArray);
 end;
-    
-implementation
 
 begin
-
+  pen.Width := 2;
+  colorsmf.AddRange(seq(clBlue, clRed, clGreen, clGoldenrod, clDarkMagenta, clDarkOrange));
 end.
--- TestFuzzy.pas
+++ TestFuzzy.pas
@@ -1,7 +1,20 @@
-﻿uses Fuzzy;
+﻿uses GraphABC, Fuzzy;
 begin
-  var f := new Trapmf('Холодно', 1, 2, 3, 4);
+ { var f := new TrapMF('Холодно', 1, 2, 3, 4);
   print(f.name, f.fun(3.2));
-  var a := new Lingvo('Температура', (10.0, 40.0), new List<TrapMF>);
-  a.funcs.Add(f);
+  var a := new Lingvar('Температура', 10, 40, new List<TrapMF>);
+  a.funcs.Add(f);}
+  {X: -1  -0.6 0 0.4   1
+   Y:  1  0.36 0 0.16  1}
+  
+  var x := new Lingvar('X', -1, 1, new List<TrapMF>);
+  x.funcs.add(new TrapMF('bn', -1, -1, -0.9, -0.8));
+  x.funcs.add(new TrapMF('n', -0.9, -0.6, -0.6, -0.4));
+  x.funcs.add(new TrapMF('z', -0.3, -0.1, 0.1, 0.3));
+  x.funcs.add(new TrapMF('p', 0.2, 0.4, 0.4, 0.6));
+  x.funcs.add(new TrapMF('bp', 0.8, 0.9, 1, 1));
+  //var y: real -> real := x.funcs[1].fun; 
+  //draw(x.funcs[1].fun, -1, 1, 0, 1);
+  x.plot;
+  //x.funcs[1].plot(-1, 1, clDarkOrange)
 end.
