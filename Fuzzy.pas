91eb1195e6347a046dce8861d8b6c3840005f018
vdjenjer <32304630+vdjenjer@users.noreply.github.com>
2018-11-19 12:50:31 +0500

    graph gridlines added

--- Fuzzy.pas
+++ Fuzzy.pas
@@ -18,6 +18,7 @@ type
     a, b: real;
     funcs: List<TrapMF>; 
     procedure plot;
+    procedure grid(nx, ny: integer);
   end;
 
 implementation
@@ -29,9 +30,11 @@ var
 
 procedure Lingvar.plot;
 begin
+  Window.Caption := 'Linguistic variable: ' + name;
+  grid(10, 4);
   for var i := 0 to funcs.Count - 1 do
   begin
-    funcs[i].plot(-1, 1, colorsmf[i mod colorsmf.Count])
+    funcs[i].plot(a, b, colorsmf[i mod colorsmf.Count])
   end;
 end;
 
@@ -54,11 +57,12 @@ begin
 end;
 
 ///Строит одну MF на отрезке [a, b]
+
 procedure TrapMF.plot(a, b: real; c: color);
 var
   Hu, Hv, rx, ry, Sx, Sy: real;
 begin
-  (rx, ry) := ((b - a) * 1.1, 1.1);
+  (rx, ry) := ((b - a) * 1.02, 1.01);
   (Sx, Sy) := (Window.Width / rx, Window.Height / ry);
   (Hu, Hv) := (-a * Sx, Window.Height - 2);
   var dx := 0.01;
@@ -72,6 +76,32 @@ begin
   end;
   pen.Color := c;
   Polyline(p.ToArray);
+end;
+
+///Строит масштабную сетку
+procedure Lingvar.grid(nx, ny: integer);
+var
+  Hu, Hv, rx, ry, Sx, Sy: real;
+begin
+  (rx, ry) := ((b - a) * 1.02, 1.01);
+  (Sx, Sy) := (Window.Width / rx, Window.Height / ry);
+  (Hu, Hv) := (-a * Sx, Window.Height - 2);
+  // Оси
+  pen.Color := clDarkGray;
+  DrawRectangle(0, 0, Window.Width, Window.Height);
+  line(0, round(Hv), Window.Width, round(Hv));
+  line(round(Hu), 0, round(Hu), Window.Height);
+  // Горизонтали
+  pen.Width := 1;
+  pen.Color := clLightGray;
+  for var i := 0 to ny - 1 do
+    line(0, round(Hv / ny * i), Window.Width, round(Hv / ny * i));
+  // Вертикали
+  var dx := (b - a) / nx;
+  var x := a;
+  for var i := 1 to nx do
+    line(round((x + dx * i) * Sx + Hu), 0, round((x + dx * i) * Sx + Hu), Window.Width);
+  pen.Width := 2;
 end;
 
 begin
